51f0e6d4c4c71d77de03091281f959fbb4ba7621
negarmohsenii <negarmohsenii@gmail.com>
2020-12-10 16:27:08 -0500

    Changes on the VEnding Machine DAO

--- VendingMachine/src/DAO/VendingMachineDAO.java
+++ VendingMachine/src/DAO/VendingMachineDAO.java
@@ -5,8 +5,13 @@
  */
 package DAO;
 
+import java.io.*;
 import DTO.Item;
 import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
 
 /**
  *
@@ -16,4 +21,39 @@ public class VendingMachineDAO implements VendingMachineDAOInterface {
     private ArrayList<Item> items;
     
     public ArrayList<Item> getItems(){ return this.items; }
+    
+    public void getAllItemsFromStorage() throws CannotOpenFile// Unmarshaling data from file to het all of the items in an ArrayList 
+    {
+        
+    }
+    
+    public void saveChangesInStorage() throws CannotOpenFile // Save all our changes in the file
+    
+    {
+        PrintWriter output;
+        try{
+            output = new PrintWriter(new FileWriter ("OutFile.txt"));
+        } catch (IOException exception) {
+            throw new CannotOpenFile("Cannot write to the file.");
+        }
+        for (Item i : items )
+        {
+            output.println(i.getName() + "::" + i.getCost() + "::" + i.getLeft());
+            output.println(i);
+        }
+        output.flush();
+        output.close();
+        
+    }
+    
+    public void updateItem(String itemName, int left) // Updates an item, we will just remove one from the stock
+    {
+    
+    }
+    
+    public Item getSpecificItem(String itemName) // Gets a specific item using it's name from the ArrayList
+    {
+
+    }
+
 }
