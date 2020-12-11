7c26312c71d1ee085a0a401219578620eb19036b
Nacer Sebti <nacersebti.ms@gmail.com>
2020-12-10 21:29:23 -0500

    WIP : Controller + View

--- VendingMachine/src/Controller/VendingMachineController.java
+++ VendingMachine/src/Controller/VendingMachineController.java
@@ -5,18 +5,50 @@
  */
 package Controller;
 
+import DAO.CannotOpenFile;
+import DTO.Item;
+import ServiceLayer.VendingMachineServiceLayer;
+import View.VendingMachineView;
+import java.util.ArrayList;
+
 /**
  *
  * @author nacer
  */
 public class VendingMachineController {
+    private VendingMachineServiceLayer service; 
+    private VendingMachineView view;
     
-    public VendingMachineController(){
-        
+    public VendingMachineController() throws CannotOpenFile{
+        this.service = new VendingMachineServiceLayer();
+        this.view = new VendingMachineView();
     }
     
     public void run(){
         // Main loop use view functions for the display
+        boolean machineOn = true;
+        float money;
+        while(machineOn){
+            ArrayList<Item> items = this.service.displayableItemDetailList();
+            displayMenu(items);
+            money = inputMoney();
+            if(money<=0){
+                machineOn = false;
+                turnOff();
+            }
+        }
+    }
+    
+    private void displayMenu(ArrayList<Item> items){      
+        this.view.displayMenu(items);
+    }
+    
+    private float inputMoney(){
+        return view.askForMoney();
+    }
+    
+    private void turnOff(){
+        
     }
     
 }
